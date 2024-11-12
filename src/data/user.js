6619a5fb3d06576a30bda2d258e807c9cec84b04
cassinihx <110645984+cassinihx@users.noreply.github.com>
2024-11-12 16:16:38 +0000

    Add files via upload

--- src/data/user.js
+++ src/data/user.js
@@ -1,28 +1,28 @@
 const INFO = {
 	main: {
-		title: "Reactfolio by truethari",
-		name: "Tharindu N.",
-		email: "mail@tharindu.dev",
+		title: "Portfolio",
+		name: "Stephen Gray",
+		email: "stephengray7505@gmail.com",
 		logo: "../logo.png",
 	},
 
 	socials: {
 		twitter: "https://twitter.com/",
-		github: "https://github.com/",
-		linkedin: "https://linkedin.com/",
-		instagram: "https://instagram.com/",
-		stackoverflow: "https://stackoverflow.com/",
-		facebook: "https://facebook.com/",
+		github: "https://github.com/cassinihx",
+		linkedin: "https://linkedin.com/in/stephengray7505",
+		instagram: "https://instagram.com/freethecloudy",
+		stackoverflow: "https://stackexchange.com/users/4122842/stephen-gray",
+		// facebook: "",
 	},
 
 	homepage: {
-		title: "Full-stack web and mobile app developer, and amateur astronaut.",
+		title: "Full-stack web app developer, Cyber Security and UX enthus.",
 		description:
 			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
 	},
 
 	about: {
-		title: "I’m Tharindu N. I live in Sri Lanka, where I design the future.",
+		title: "I'm Stephen, I live in Bristol where I work on breaking the barrier between engineer and user.",
 		description:
 			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
 	},
